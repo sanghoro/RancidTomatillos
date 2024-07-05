e7044935545d41082c6aaab9d52d00c8ffc223de
Adam Konber <konber3@gmail.com>
2024-07-05 10:02:03 -0600

    Created Footer component and its styles. Co-authored-by: Seong-Kang <seong.h.kang2@gmail.com> Co-authored-by: Adam-Konber <konber3@gmail.com>

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../componentStyles/Footer.css'
+
+const Footer = () => {
+  return (
+<footer className='footers'>
+    <h1>Rancid Tomatillos</h1>
+</footer>
+  )
+}
+
+export default Footer
