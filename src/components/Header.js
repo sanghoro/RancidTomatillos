76b2d08c957d968e8802602c8a7b5db6d1f0a9cc
Adam Konber <konber3@gmail.com>
2024-07-05 09:56:20 -0600

    Created Header component and styles for component Co-authored-by: Adam-Konber <konber3@gmail.com> Co-authored-by: Seong-Kang <seong.h.kang2@gmail.com>

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './Nav'
+import '../componentStyles/Header.css'
+
+const Header = () => {
+  return (
+    <header className='header'>
+        <Nav />
+    </header>
+  )
+}
+
+export default Header
