2d35c656ea4fc47275b355ec491295d1e2010702
Seong Kang <seong.h.kang2@gmail.com>
2024-07-05 20:36:45 -0500

    fetched the movies data from backend successfully

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import MockData from './MockData/mock-data'
-import { useState } from 'react';
+// import MockData from './MockData/mock-data'
+import { useState, useEffect } from 'react';
 
 function App() {
-const [movies, setMovies] = useState(MockData.movies)
+const [movies, setMovies] = useState([])
+
+function getMovieData(){
+  fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
+  .then(response => response.json())
+  .then(data => {
+    setMovies(data.movies)
+    console.log('fetched movie data', data.movies
+    )})
+  .catch(err => console.log(err))
+}
+
+useEffect(()=> {
+  getMovieData()
+}, [])
+
 
   return (
     <div className='App'>
