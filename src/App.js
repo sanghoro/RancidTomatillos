388296e3e5ea23c5644ce792b42c55873fe837b2
Seong Kang <seong.h.kang2@gmail.com>
2024-07-05 20:51:59 -0500

    added more fetch functions for single movie data and trailer

--- src/App.js
+++ src/App.js
@@ -8,18 +8,39 @@ import { useState, useEffect } from 'react';
 function App() {
 const [movies, setMovies] = useState([])
 
+//All Fetch
 function getMovieData(){
   fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
   .then(response => response.json())
   .then(data => {
-    setMovies(data.movies)
-    console.log('fetched movie data', data.movies
-    )})
+    setMovies(data.movies)})
+  .catch(err => console.log(err))
+}
+function getSingleMovieData(id){
+  fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+  .then(response => response.json())
+  .then(data => console.log('fetched single movie data', data))
   .catch(err => console.log(err))
 }
 
+function getSingleMovieTrailer(id){
+  fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}/videos`)
+  .then(response => response.json())
+  .then(data => console.log('fetched movie trailer', data))
+  .catch(err => console.log(err))
+}
+
+//All useEffects
 useEffect(()=> {
   getMovieData()
+}, [])
+
+useEffect(()=> {
+  getSingleMovieData(436270)
+}, [])
+
+useEffect(()=> {
+  getSingleMovieTrailer(436270)
 }, [])
 
 
