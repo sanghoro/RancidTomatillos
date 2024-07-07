e850182e96b626e57a7e30fd398cc573cf17d154
Adam Konber <konber3@gmail.com>
2024-07-07 15:34:16 -0600

    Added propTypes to Header.js and styles to header.css to adjust background image.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import Nav from './Nav'
 import '../componentStyles/Header.css'
+import PropTypes from 'prop-types'
 
 const Header = ( { movie }) => {
   return (
-    <header className='header' style={{ 
-      backgroundImage: `url(${movie?.backdrop_path})`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center'
+    <header className='movie-header' style={{ 
+      backgroundImage: `url(${movie?.backdrop_path})`
+      
     }}>
         <Nav />
     </header>
   )
+}
+
+Header.PropTypes = {
+  movie: PropTypes.shape({
+    backdrop_path: PropTypes.string.isRequired
+  })
 }
 
 export default Header
