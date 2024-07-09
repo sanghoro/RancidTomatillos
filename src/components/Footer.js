e711c037654a64cde4c8640174d6ead466e8b4ff
Seong Kang <seong.h.kang2@gmail.com>
2024-07-09 16:57:39 -0500

    added footer design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import '../componentStyles/Footer.css'
+import github from '../img/github-Icon.png'
 
 const Footer = () => {
   return (
 <footer className='footers'>
     <h1>Rancid Tomatillos</h1>
+    <div className="Info-section">
+      <h3 className='name'>Adam Konber</h3>
+        <img className="github" src={github} />
+        <a href="https://github.com/Sterling47" className="Adam-Github"> @Github</a>
+
+        <h3 className='name'>Seong Kang</h3>
+        <img className="github" src={github} />
+        <a href='https://github.com/Sanghoro' className="Seong-Github"> @Github</a>
+    </div>
+
 </footer>
   )
 }
