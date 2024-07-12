0f97d97cd576f249faceed75fb0f89ec52030512
Adam Konber <konber3@gmail.com>
2024-07-12 16:22:06 -0600

    Added motion element ot div to handle image transition in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import Nav from './Nav';
+import { motion } from 'framer-motion';
 import '../componentStyles/Header.css';
 import PropTypes from 'prop-types';
 
 const Header = ({ movie }) => {
   return (
-    <header className='movie-header' style={{ 
-      backgroundImage: `url(${movie?.backdrop_path})`
-    }}>
+    <header className='movie-header'>
+      <motion.div 
+        className='movie-header' 
+        key={movie?.backdrop_path} // Adding key to trigger re-animation on movie change
+        style={{ 
+          backgroundImage: `url(${movie?.backdrop_path})` 
+        }} 
+        initial={{ opacity: 0.5, x: 100 }} // Start off-screen to the right
+        animate={{ opacity: 1, x: 0 }} // Slide in to the center
+        exit={{ opacity: 0, x: -100 }} // Slide out to the left
+        transition={{ duration: 1.3 }} // Duration of the animation
+      />
       <Nav />
       {movie && (
         <div className="movie-title-container">
